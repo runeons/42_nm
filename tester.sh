0632c0d216c87ded4d797bdf30ccad3d38da0bb2
Tess SANTONI <tsantoni@bess-f4r6s2.clusters.42paris.fr>
2024-03-18 10:23:22 +0100

    tester ready

--- tester.sh
+++ tester.sh
@@ -10,10 +10,27 @@ compare()
 }
 
 FILES=(
+    # "/usr/bin/docker-proxy"
+    "./bin/ft_nm"
+    "./bin/easy32"
     "./bin/easy_test"
-    "./bin/error_header"
     "./bin/header"
+    "./bin/header_and_prog_copy"
+    "./bin/header_offset_error"
+    "./bin/not_so_easy_test32.o"
+    # "./bin/not_so_easy_test64_obj.o"
+    # "./bin/unterminated_string"
+    # "./bin/easy64"
+    # "./bin/error_header"
+    # "./bin/header_and_prog"
+    # "./bin/header_cop"
+    # "./bin/not_so_easy_test"
+    # "./bin/not_so_easy_test32_obj.o"
+    # "./bin/not_so_easy_test.owrong_arch"
 )
+
+# FILES+=(./bin/*)
+
 
 if [ $# -eq 1 ]; then
     compare "$1"
